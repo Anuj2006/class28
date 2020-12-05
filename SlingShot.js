246c422c5ab9405332bffcc2619df2d1e33c14b8
Anuj2006 <71918724+Anuj2006@users.noreply.github.com>
2020-12-05 12:36:40 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,28 @@
+class SlingShot{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            length:10,
+            stiffness:0.05
+        }
+
+        this.pointB=pointB;
+        this.chain = Constraint.create(options);
+        World.add(world,this.chain);
+    }
+
+    display(){
+        if(this.chain.bodyA){
+        var pointA=this.chain.bodyA.position;
+        var pointB=this.pointB;
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+
+    }
+
+    fly (){
+        this.chain.bodyA=null;      
+    }
+}
+
